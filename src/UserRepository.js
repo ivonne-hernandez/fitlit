0c3be5ecd8601f2c0c56f7aaa8521478ed4abc2b
Ivonne Hernandez <ivonne702@gmail.com>
2021-10-16 14:49:00 -0600

    Add a constructor and renderUserData method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,11 @@
 class UserRepository {
+  constructor(data) {
+    this.data = data
+  }
 
+  renderUserData(userId) {
+    return this.data.find((user) => user.id === userId);
+  }
 }
 
 export default UserRepository;
