165ffb0b5bf31ea3f035115dea7abd9c0ca152c1
Ivonne Hernandez <ivonne702@gmail.com>
2021-10-16 18:34:16 -0600

    Add tests for the User class properties and renderUserFirstName method

--- test/User-test.js
+++ test/User-test.js
@@ -31,4 +31,37 @@ describe('User', () => {
     expect(user).to.be.an.instanceOf(User);
   });  
 
+  it('should have an id', function() {
+    expect(user.id).to.equal(1);
+  });
+
+  it('should have a name', function() {
+    expect(user.name).to.equal("Luisa Hane");
+  });
+
+  it('should have an address', function() {
+    expect(user.address).to.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697");
+  });
+
+  it('should have an email', function() {
+    expect(user.email).to.equal("Diana.Hayes1@hotmail.com");
+  });
+
+  it('should have a strideLength', function() {
+    expect(user.strideLength).to.equal(4.3);
+  });
+
+  it('should have a dailyStepGoal', function() {
+    expect(user.dailyStepGoal).to.equal(10000);
+  });
+
+  it('should have friends', function() {
+    expect(user.friends).to.deep.equal([16, 4, 8]);
+  });
+
+  it('should return a user\'s first name', function() {
+    const result = user.renderUserFirstName();
+    expect(result).to.equal("Luisa Hane");
+  });
+
 });
