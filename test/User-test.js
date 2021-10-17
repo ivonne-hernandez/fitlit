7c27522ed23ef6e8722f93da2bbf3b9cf8d69af0
Ivonne Hernandez <ivonne702@gmail.com>
2021-10-16 18:18:03 -0600

    Add a function and instance testt for the User class

--- test/User-test.js
+++ test/User-test.js
@@ -1,26 +1,34 @@
-// const chai = require('chai');
-// const expect = chai.expect;
+import { expect } from 'chai';
+import User from '../src/User.js';
 
-// const User = require('../src/User');
+describe('User', () => {
+  let userData;
+  let user;
 
-// describe('User', function() {
-//   let userData;
+  beforeEach(function() {
+    userData =
+      {
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+          16,
+          4,
+          8
+        ]
+      };
+    user = new User(userData);
+  });
 
-//   beforeEach(function() {
-//     userData =
-//       {
-//         "id": 1,
-//         "name": "Luisa Hane",
-//         "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-//         "email": "Diana.Hayes1@hotmail.com",
-//         "strideLength": 4.3,
-//         "dailyStepGoal": 10000,
-//         "friends": [
-//           16,
-//           4,
-//           8
-//         ]
-//       }
+  it('should be a function', function () {
+    expect(User).to.be.a('function');
+  });
 
+  it('should be an instance of User', function() {
+    expect(user).to.be.an.instanceOf(User);
+  });  
 
-// });
+});
