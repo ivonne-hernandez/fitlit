f4bf9aee6f88e3196f8d1584315db2263f1f89c2
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-10-20 15:51:18 -0600

    Add beforeEach for SleepRepository-test.js file

--- test/SleepRepository-test.js
+++ test/SleepRepository-test.js
@@ -1,5 +1,31 @@
 import { expect } from 'chai';
-import Sleep from '../src/Sleep.js';
-describe('Sleep', () => {
-  
+import SleepRepository from '../src/SleepRepository.js';
+
+describe('Sleep Repository', () => {
+  let sleepData;
+  let sleepRepository;
+
+  beforeEach(function() {
+    sleepData =
+      [
+        {
+          "userID": 1,
+          "date": "2019/06/15",
+          "hoursSlept": 6.1,
+          "sleepQuality": 2.2
+        },
+        {
+          "userID": 2,
+          "date": "2019/06/15",
+          "hoursSlept": 7,
+          "sleepQuality": 4.7
+        },
+        {
+          "userID": 3,
+          "date": "2019/06/15",
+          "hoursSlept": 10.8,
+          "sleepQuality": 4.7
+        }
+      ]
+  })
 })
