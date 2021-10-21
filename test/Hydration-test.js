84ba5b442b2af6a60996c105549472d10d099289
August Reid <c.august.reid@gmail.com>
2021-10-21 15:21:07 -0600

    Add data set for Hydration tests

---
+++ test/Hydration-test.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import Hydration from '../src/Hydration';
+
+describe('User hydration data', () => {
+  let data;
+  let userHydration;
+
+  beforeEach(function() {
+    data = [
+      {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numOunces": 37
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/16",
+      "numOunces": 75
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/17",
+      "numOunces": 69
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/18",
+      "numOunces": 91
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/19",
+      "numOunces": 38
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/20",
+      "numOunces": 79
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/21",
+      "numOunces": 69
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/22",
+      "numOunces": 91
+      }];
+
+    userHydration = new Hydration(data)
+  });
