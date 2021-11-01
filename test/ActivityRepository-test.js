3732362540e067176676b69b72c51ffe5d26228f
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-11-01 15:29:39 -0600

    Add ActivityRepository-test.js file

---
+++ test/ActivityRepository-test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import ActivityRepository from '../src/ActivityRepository';
+describe('ActivityRepository', () => {
+  let userActivity;
+  let activityRepository;
+  let userId;
+
+  beforeEach(function() {
+    activityData = [
+      
+    ];
+  })
+})
