91aa21dc47c859f93a1098247e693a24a1bbc83a
August Reid <c.august.reid@gmail.com>
2021-11-05 16:46:17 -0600

    Enable submit button when all form inputs are valid

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -3,15 +3,18 @@ let activityDateInput = document.querySelector('#addActivityDate');
 let activityStepsInput = document.querySelector('#addDataSteps');
 let activityStairsInput = document.querySelector('#addDataStairs');
 let activityMinutesInput = document.querySelector('#addDataMinutes')
-
+let activityInputForm = document.querySelector('#activityInputForm')
 
 
 const validateUserInput = () => {
   if (activityDateInput.value && activityStepsInput.value && activityStairsInput.value && activityMinutesInput.value) {
     activitySubmitButton.disabled = false;
+  } else {
+    activitySubmitButton.disabled = true;
   }
 }
 
+activityInputForm.addEventListener("keyup", validateUserInput)
 
 
 
