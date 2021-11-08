2a8dc1eca35962e408ae7d7a704fabf8f5d2e07f
Ivonne Hernandez <ivonne702@gmail.com>
2021-11-08 11:30:13 -0700

    Remove comments

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,3 @@
-//here is where our API fetch calls will happen
-//export the functions
-
-// let fetchUserData = () => {
-//   fetch("https://pacific-badlands-43237.herokuapp.com/api/v1/users").then(response => response.json()).then(data => data.userData);
-// }
-
 export let fetchUserData = () => {
   return fetch("http://localhost:3001/api/v1/users").then(response => response.json())
 }
